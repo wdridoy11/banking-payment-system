ed4d65b88f8ccfce00c6d0f06ed5f50fcd3c2977
Ridoy Sharif <developersridoy@gmail.com>
2023-02-12 19:52:49 +0600

    previous withdraw balance  and new withdraw balance subtraction complete

--- js/withdraw.js
+++ js/withdraw.js
@@ -7,13 +7,25 @@ withdrawBtn.addEventListener("click", function () {
    let withdrawField = document.getElementById("withdraw-field");
    let withdrawFieldString = withdrawField.value;
    let newWithdrawAmount = parseFloat(withdrawFieldString);
+
    // previous withdraw amount
    let withdrawTotal = document.getElementById("withdraw-total");
    let withdrawTotalString = withdrawTotal.innerText;
    let previousWithdrawAmount = parseFloat(withdrawTotalString);
+
    // new withdraw amount and previous withdraw amount adding
    let currentWithdrawAmount = previousWithdrawAmount + newWithdrawAmount;
    withdrawTotal.innerText = currentWithdrawAmount;
+
+   // previous balance
+   let balanceTotal = document.getElementById("balance-total");
+   let balanceTotalString = balanceTotal.innerText;
+   let previousBalance = parseFloat(balanceTotalString);
+
+   // previous balance and new withdraw amount subtraction
+   let currentBalanceTotal = previousBalance - newWithdrawAmount;
+   balanceTotal.innerText = currentBalanceTotal;
+
    // when withdraw amount adding completes then clear input field
    withdrawField.value = "";
 })
