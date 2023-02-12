7dd2e64003db444b7fb7a5199e6a590ab079bc4b
Ridoy Sharif <developersridoy@gmail.com>
2023-02-12 17:47:39 +0600

    Login system is complete. Now I will work in bank in system

--- js/login.js
+++ js/login.js
@@ -6,11 +6,12 @@ submitBtn.addEventListener("click", function () {
    // user input value
    let userEmail = document.getElementById("user-email");
    let userEmailValue = userEmail.value;
+   console.log(userEmailValue)
    let userPassword = document.getElementById("user-password");
    let userPasswordValue = userPassword.value;
    // user input validation check valid user
-   if (userEmailValue === "wdridoy11@gmail.com") {
-      window.location.href = "../index.html";
+   if (userEmailValue === "wdridoy11@gmail.com" && userPasswordValue === "2222") {
+      window.location.href = "../bank.html";
    } else {
       alert("Please send valid information. Please try again");
    }
