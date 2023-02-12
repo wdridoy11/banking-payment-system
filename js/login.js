ff3c8ad856e8253cf92dbcef2fa5385a909b5054
Ridoy Sharif <developersridoy@gmail.com>
2023-02-12 16:55:26 +0600

    login system complete

--- js/login.js
+++ js/login.js
@@ -1,7 +1,17 @@
+/*==================================================
+               Login system
+  ==================================================*/
 const submitBtn = document.getElementById("submit-btn");
-submitBtn.addEventListener('click', function () {
+submitBtn.addEventListener("click", function () {
+   // user input value
    let userEmail = document.getElementById("user-email");
    let userEmailValue = userEmail.value;
    let userPassword = document.getElementById("user-password");
    let userPasswordValue = userPassword.value;
+   // user input validation check valid user
+   if (userEmailValue === "wdridoy11@gmail.com") {
+      window.location.href = "../index.html";
+   } else {
+      alert("Please send valid information. Please try again");
+   }
 })
