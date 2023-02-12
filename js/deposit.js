d405731ff28a45b88dbfe053a87bcc7f7629ad31
Ridoy Sharif <developersridoy@gmail.com>
2023-02-12 19:37:35 +0600

    code beautify and file name change

--- js/deposit.js
+++ js/deposit.js
@@ -7,20 +7,25 @@ depositBtn.addEventListener("click", function () {
   let depositInputField = document.getElementById("deposit-field");
   let depositAmountString = depositInputField.value;
   let newDepositAmount = parseFloat(depositAmountString);
+
   // previous deposit amount
   let depositTotal = document.getElementById("deposit-total");
   let depositTotalString = depositTotal.innerText;
   let previousDepositAmount = parseFloat(depositTotalString);
+
   // previous deposit amount and new deposit amount adding
   let currentDepositAmount = previousDepositAmount + newDepositAmount;
   depositTotal.innerText = currentDepositAmount;
+
   // previous balance
   let balanceTotal = document.getElementById("balance-total");
   let balanceTotalString = balanceTotal.innerText;
   let previousBalanceTotal = parseFloat(balanceTotalString);
+
   // previous balance and new deposit balance adding 
   let currentBalanceTotal = previousBalanceTotal + newDepositAmount;
   balanceTotal.innerText = currentBalanceTotal;
+
   // when new deposit amount adding completes then create input field
   depositInputField.value = "";
 })
