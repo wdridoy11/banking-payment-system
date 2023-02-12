18ae475d0d8a09cb99d351eae66548b87f490ad6
Ridoy Sharif <developersridoy@gmail.com>
2023-02-12 16:46:32 +0600

    Now I am create login system

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener('click', function () {
+   let userEmail = document.getElementById("user-email");
+   let userEmailValue = userEmail.value;
+   let userPassword = document.getElementById("user-password");
+   let userPasswordValue = userPassword.value;
+})
